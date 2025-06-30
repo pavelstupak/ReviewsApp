fff8a02cde717d093c135d589052d8af6b46d67a
Pavel Stupak <pavstupak@gmail.com>
2025-06-30 23:49:25 +0300

    Enhance Logger to include timestamp and log levels

--- ReviewsApp/Utils/Logger.swift
+++ ReviewsApp/Utils/Logger.swift
@@ -1,37 +1,32 @@
 import Foundation
 
+enum LogLevel: String {
+	case debug = "DEBUG"
+	case info = "INFO"
+	case warn = "WARNING"
+	case error = "ERROR"
+}
+
 final class Logger {
-	
+
+	// Общий экземпляр Singleton
 	static let shared = Logger()
 
 	private init() {}
 
-	func debug(_ message: String) {
-		#if DEBUG
-		print(message)
-		#endif
+	func log(_ message: String, level: LogLevel) {
+		let timestamp = formattedTimestamp()
+		print("[\(timestamp)] [\(level.rawValue)] \(message)")
 	}
 
-	func info(_ message: String) {
-		#if DEBUG
-		print(message)
-		#endif
-	}
+	func debug(_ message: String) { log(message, level: .debug) }
+	func info(_ message: String) { log(message, level: .info) }
+	func warn(_ message: String) { log(message, level: .warn) }
+	func error(_ message: String) { log(message, level: .error) }
 
-	func warn(_ message: String) {
-		#if DEBUG
-		print(message)
-		#else
-		// ToDo: Отправка в систему логирования
-		#endif
+	private func formattedTimestamp() -> String {
+		let formatter = DateFormatter()
+		formatter.dateFormat = "HH:mm:ss.SSS"
+		return formatter.string(from: Date())
 	}
-
-	func error(_ message: String) {
-		#if DEBUG
-		print(message)
-		#else
-		// ToDo: Отправка crash-репорта
-		#endif
-	}
-
 }
